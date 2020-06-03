256d8ce623adf673a3b9ec7cf4381d59190e3f5b
dev-harsh1998 <god@harsh1998.me>
2020-06-03 14:58:13 +0200

    RMX1921: adapt brightness for aosp values

--- device.mk
+++ device.mk
@@ -241,8 +241,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.realme_sdm710 \
-    lights.sdm710
+    android.hardware.light@2.0-service.xt
 
 # Media
 PRODUCT_COPY_FILES += \
